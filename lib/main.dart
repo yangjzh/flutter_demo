d808a1e138f98bca47f4fbf0d62dde6f0c6fa7a1
yangjz01 <yangjzh01@gmail.com>
2019-03-21 19:14:33 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,17 @@
 import 'package:flutter/material.dart';
-import './pages/home_screen.dart';
-import './pages/emai_screen.dart';
-import './pages/pages_screen.dart';
-import './pages/airplay_screen.dart';
+import './pages/bottom_appBar_demo.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My App",
-      theme: ThemeData.light(),
-      home: BottomNavigation(),
-    );
-  }
-}
-
-class BottomNavigationWidget extends StatefulWidget {
-  _BottomNavigationWidgetState createState() => _BottomNavigationWidgetState();
-}
-
-class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
-  final _BottomNavigationColor = Colors.blue;
-  int _currentIndex = 0;
-  List<Widget> list = List();
-  @override
-  void initState() {
-    list
-      ..add(HomeScreen())
-      ..add(EmailScreen())
-      ..add(PagesScreen())
-      ..add(AirplayScreen());
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: list[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  color: _BottomNavigationColor,
-                ),
-                title: Text('Home',
-                    style: TextStyle(color: _BottomNavigationColor))),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.email,
-                  color: _BottomNavigationColor,
-                ),
-                title: Text('Email',
-                    style: TextStyle(color: _BottomNavigationColor))),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.pages,
-                  color: _BottomNavigationColor,
-                ),
-                title: Text('Pages',
-                    style: TextStyle(color: _BottomNavigationColor))),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.airplay,
-                  color: _BottomNavigationColor,
-                ),
-                title: Text('AipPlay',
-                    style: TextStyle(color: _BottomNavigationColor))),
-          ],
-          currentIndex: _currentIndex,
-          onTap: (int index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          type: BottomNavigationBarType.fixed),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: BottomAppBarDemo(),
     );
   }
 }
